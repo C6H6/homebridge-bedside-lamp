a91a3fe128beb7882b6deddfdc9244ee89cd6763
Patryk Wieczorek <c3c3h3h3@gmail.com>
2020-01-03 17:45:49 +0100

    Add characteristics

--- index.js
+++ index.js
@@ -18,25 +18,90 @@ BedsideLamp.prototype = {
     getServices: function () {
         let informationService = new Service.AccessoryInformation();
 
-        let switchService = new Service.Switch;
-        switchService
+        let LightbulbService = new Service.Lightbulb;
+
+        LightbulbService
             .getCharacteristic(Characteristic.On)
             .on('get', this.getOnCharacteristicHandler.bind(this))
             .on('set', this.setOnCharacteristicHandler.bind(this));
 
-        this.informationService = informationService;
-        this.switchService = switchService;
-        return [informationService, switchService];
+        LightbulbService
+            .getCharacteristic(Characteristic.Brightness)
+            .on('get', this.getBrightnessCharacteristicHandler.bind(this))
+            .on('set', this.setBrightnessCharacteristicHandler.bind(this));
+
+        LightbulbService
+            .getCharacteristic(Characteristic.Hue)
+            .on('get', this.getHueCharacteristicHandler.bind(this))
+            .on('set', this.setHueCharacteristicHandler.bind(this));
+
+        LightbulbService
+            .getCharacteristic(Characteristic.Saturation)
+            .on('get', this.getSaturationCharacteristicHandler.bind(this))
+            .on('set', this.setSaturationCharacteristicHandler.bind(this));
+
+        LightbulbService
+            .getCharacteristic(Characteristic.ColorTemperature)
+            .on('get', this.getColorTemperatureCharacteristicHandler.bind(this))
+            .on('set', this.setColorTemperatureCharacteristicHandler.bind(this));
+
+        // this.informationService = informationService;
+        // this.LightbulbService = LightbulbService;
+        return [informationService, LightbulbService];
     },
 
     getOnCharacteristicHandler(callback) {
-        this.log("Called get");
+        this.log("Called get on");
         callback(null, status)
     },
 
     setOnCharacteristicHandler(value, callback) {
-        this.log("Called get. Value: " + value);
+        this.log("Called set on. Value: " + value);
         status = value;
         callback(null)
-    }
+    },
+
+    getBrightnessCharacteristicHandler(callback) {
+        this.log("Called get brightness");
+        callback(null, status)
+    },
+
+    setBrightnessCharacteristicHandler(value, callback) {
+        this.log("Called set brightness. Value: " + value);
+        status = value;
+        callback(null)
+    },
+
+    getSaturationCharacteristicHandler(callback) {
+        this.log("Called get hue");
+        callback(null, status)
+    },
+
+    setSaturationCharacteristicHandler(value, callback) {
+        this.log("Called set hue. Value: " + value);
+        status = value;
+        callback(null)
+    },
+
+    getHueCharacteristicHandler(callback) {
+        this.log("Called get hue");
+        callback(null, status)
+    },
+
+    setHueCharacteristicHandler(value, callback) {
+        this.log("Called set hue. Value: " + value);
+        status = value;
+        callback(null)
+    },
+
+    getColorTemperatureCharacteristicHandler(callback) {
+        this.log("Called get color/temperature");
+        callback(null, status)
+    },
+
+    setColorTemperatureCharacteristicHandler(value, callback) {
+        this.log("Called set color/temperature. Value: " + value);
+        status = value;
+        callback(null)
+    },
 };
